900ebd688e076261d05b4a46bc15869a1809be6b
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-26 21:18:46 +0300

    Add legend to GeneralInformation

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -3,7 +3,10 @@ import React from 'react';
 class GeneralInformation extends React.Component {
   render() {
     return (
-      <fieldset className="c-fieldset">{this.props.children}</fieldset>
+      <fieldset className="c-fieldset">
+        <legend className="c-fieldset__legend">General information</legend>
+        {this.props.children}
+      </fieldset>
     );
   }
 }
