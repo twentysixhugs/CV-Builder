04c308b927abc26fe46a65f203ec44dca5b98ce3
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-27 16:06:32 +0300

    Add constructors for Education and Experience

--- src/modules/fieldsetCreator.js
+++ src/modules/fieldsetCreator.js
@@ -2,6 +2,17 @@ export function SkillsFieldsetData() {
   this.skillInfo = '';
 }
 
-export function EducationFieldsetData() {}
+export function EducationFieldsetData() {
+  this.schoolName = '';
+  this.titleOfStudy = '';
+  this.from = '';
+  this.to = '';
+}
 
-export function ExperienceFieldsetData() {}
+export function ExperienceFieldsetData() {
+  this.companyName = '';
+  this.position = '';
+  this.mainTasks = '';
+  this.from = '';
+  this.to = '';
+}
