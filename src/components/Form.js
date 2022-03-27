1c3690968b0943ac7534d788bf8d39e566064d33
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-27 15:38:02 +0300

    Add handler for new input fieldset creation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,33 @@
 import { toHaveFocus } from '@testing-library/jest-dom/dist/matchers';
 import React from 'react';
 import GeneralInformation from './GeneralInformation';
-import Skill from './Skill';
+import Skills from './Skills';
+
+import {
+  SkillsFieldsetData,
+  EducationFieldsetData,
+  ExperienceFieldsetData,
+} from '../modules/fieldsetCreator';
 
 class Form extends React.Component {
   constructor(props) {
     super(props);
 
+    /* Each array represents a key-value pair stored in an object */
+    /* On input change, the objects are updated (with deep cloning and setState) */
+    /* On creating a new input field, it is added here. So, the form is dynamic */
+    /* An array is passed to form fieldsets as inputFields */
+
     this.state = {
       general: [{ firstname: '', lastname: '', email: '', phone: '' }],
-      skills: [
-        { skillInfo: 'I can hug' },
-        { skillInfo: 'I can write code' },
-        { skillInfo: 'I am self-disciplined' },
-      ],
+      skills: [],
       education: [],
       experience: [],
     };
 
     this.onSubmit = this.onSubmit.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
+    this.onNewInputCreation = this.onNewInputCreation.bind(this);
   }
 
   onSubmit(e) {
@@ -29,15 +37,43 @@ class Form extends React.Component {
   }
 
   onInputChange(index, e, updatedArrayName) {
-    /* Create a deep copy of the array of input key-value pairs */
-    const deepCopy = JSON.parse(
-      JSON.stringify(this.state[updatedArrayName]),
-    );
+    /* Create a deep copy of inputField key-value pairs */
+    const deepCopy = this.#getDeepCopy(updatedArrayName);
+
     /* Update the necessary key-value pair */
     deepCopy[index][e.target.name] = e.target.value;
 
     /* Replace the original array in state with the updated one */
     this.setState({ [updatedArrayName]: deepCopy });
+  }
+
+  onNewInputCreation(updatedArrayName, e) {
+    /* Create a deep copy of inputField key-value pairs */
+    const deepCopy = this.#getDeepCopy(updatedArrayName);
+
+    /* Define which fieldset to create */
+    let newInputFields;
+
+    if (updatedArrayName === 'skills') {
+      newInputFields = new SkillsFieldsetData();
+    }
+    if (updatedArrayName === 'education') {
+      newInputFields = new EducationFieldsetData();
+    }
+    if (updatedArrayName === 'experience') {
+      newInputFields = new ExperienceFieldsetData();
+    }
+
+    deepCopy.push(newInputFields);
+
+    /* Replace the original array in state with the updated one */
+    this.setState({ [updatedArrayName]: deepCopy });
+  }
+
+  #getDeepCopy(propName) {
+    const deepCopy = JSON.parse(JSON.stringify(this.state[propName]));
+
+    return deepCopy;
   }
 
   render() {
