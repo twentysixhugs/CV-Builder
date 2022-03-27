a620a38575b278ab54413f6189b5928a38b9a32b
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-27 15:38:33 +0300

    Build Skills

---
+++ src/components/Skills.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Field from './Field';
+
+class Skills extends React.Component {
+  render() {
+    const { inputFields } = this.props;
+
+    return (
+      <div className="c-fieldset c-fieldset--skills">
+        <legend className="c-fieldset__legend">Skills</legend>
+        {inputFields.map((field, index) => {
+          return (
+            <div key={index} className="fields-wrapper">
+              <Field
+                labelText="Skill"
+                type="text"
+                id={`skill-${index}`}
+                name={`skillInfo`}
+                value={field.skillInfo}
+                onChange={(e) =>
+                  this.props.onInputChange(index, e, 'skills')
+                }
+              />
+            </div>
+          );
+        })}
+        <button
+          onClick={(e) => this.props.onNewInputCreation('skills', e)}
+          className="c-fieldset__button"
+        >
+          Add...
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Skills;
