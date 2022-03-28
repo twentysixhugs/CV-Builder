e397437711b5ce7b12275fb423327b40ed16d479
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-28 14:38:08 +0300

    Add textarea conditional render in Field

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,19 +2,38 @@ import React from 'react';
 
 class Field extends React.Component {
   render() {
-    return (
-      <div className="c-field">
-        <label className="c-field__label" htmlFor={this.props.id}>
-          {this.props.labelText}
-        </label>
-        <input
+    let inputToRender = (
+      <input
+        className="c-field__input"
+        type={this.props.type}
+        id={this.props.id}
+        name={this.props.name}
+        value={this.props.value}
+        onChange={this.props.onChange}
+        required={this.props.required}
+      ></input>
+    );
+
+    if (this.props.textArea) {
+      inputToRender = (
+        <textarea
           className="c-field__input"
           type={this.props.type}
           id={this.props.id}
           name={this.props.name}
           value={this.props.value}
           onChange={this.props.onChange}
-        ></input>
+          required={this.props.required}
+        ></textarea>
+      );
+    }
+
+    return (
+      <div className="c-field">
+        <label className="c-field__label" htmlFor={this.props.id}>
+          {this.props.labelText}
+          {inputToRender}
+        </label>
       </div>
     );
   }
