7cfbaebe88aca1bcfbf8665a0a4a87223dd89fdd
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-03-31 16:02:01 +0300

    Refactor App with hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/App.css';
 import React from 'react';
+import { useState, useEffect } from 'react';
 import DynamicForm from './components/DynamicForm';
 import Result from './components/Result';
 
@@ -9,45 +10,43 @@ import {
   ExperienceFieldsetData,
 } from './modules/fieldsetCreator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+function App() {
+  /* Each array represents a key-value pair stored in an object */
+  /* On input change, the objects are updated (with deep cloning and setState) */
+  /* On creating a new input field, it is added here. So, the form is dynamic */
+  /* An array is passed to form as fieldsets */
 
-    /* Each array represents a key-value pair stored in an object */
-    /* On input change, the objects are updated (with deep cloning and setState) */
-    /* On creating a new input field, it is added here. So, the form is dynamic */
-    /* An array is passed to form as fieldsets */
+  // this.handleInputChange = this.handleInputChange.bind(this);
+  // this.handleNewInputCreation = this.handleNewInputCreation.bind(this);
+  // this.handleRemoveInput = this.handleRemoveInput.bind(this);
+  // this.handleSubmit = this.handleSubmit.bind(this);
+  // this.handleResultEdit = this.handleResultEdit.bind(this);
 
-    this.state = {
-      general: [{ firstName: '', lastName: '', email: '', phone: '' }],
-      skills: [],
-      education: [],
-      experience: [],
-      isDisplayingForm: true,
-      isSubmitDisabled: true,
-    };
+  const [fieldsets, setFieldsets] = useState({
+    general: [{ firstName: '', lastName: '', email: '', phone: '' }],
+    skills: [],
+    education: [],
+    experience: [],
+  });
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleNewInputCreation = this.handleNewInputCreation.bind(this);
-    this.handleRemoveInput = this.handleRemoveInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleResultEdit = this.handleResultEdit.bind(this);
-  }
+  const [isDisplayingForm, setIsDisplayingForm] = useState(true);
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
 
-  handleInputChange(index, e, updatedArrayName) {
+  function handleInputChange(index, e, updatedArrayName) {
     /* Create a deep copy of inputField key-value pairs */
-    const deepCopy = this.#getDeepCopy(updatedArrayName);
+    const deepCopy = _getDeepFieldsetCopy(updatedArrayName);
 
     /* Update the necessary key-value pair */
     deepCopy[index][e.target.name] = e.target.value;
 
     /* Replace the original array in state with the updated one */
-    this.setState({ [updatedArrayName]: deepCopy });
+    // this.setState({ [updatedArrayName]: deepCopy });
+    setFieldsets({ ...fieldsets, [updatedArrayName]: deepCopy });
   }
 
-  handleNewInputCreation(updatedArrayName, e) {
+  function handleNewInputCreation(updatedArrayName, e) {
     /* Create a deep copy of inputField key-value pairs */
-    const deepCopy = this.#getDeepCopy(updatedArrayName);
+    const deepCopy = _getDeepFieldsetCopy(updatedArrayName);
 
     /* Define which fieldset to create */
     let newInputFields;
@@ -65,36 +64,36 @@ class App extends React.Component {
     deepCopy.push(newInputFields);
 
     /* Replace the original array in state with the updated one */
-    this.setState({ [updatedArrayName]: deepCopy });
+    setFieldsets({ ...fieldsets, [updatedArrayName]: deepCopy });
   }
 
-  handleRemoveInput(index, updatedArrayName, e) {
+  function handleRemoveInput(index, updatedArrayName, e) {
     /* Create a deep copy of inputField key-value pairs */
-    const deepCopy = this.#getDeepCopy(updatedArrayName);
+    const deepCopy = _getDeepFieldsetCopy(updatedArrayName);
 
     /* Remove the necessary key-value pair */
     deepCopy.splice(index, 1);
 
     /* Replace the original array in state with the updated one */
-    this.setState({ [updatedArrayName]: deepCopy });
+    setFieldsets({ ...fieldsets, [updatedArrayName]: deepCopy });
   }
 
-  #getDeepCopy(propName) {
-    const deepCopy = JSON.parse(JSON.stringify(this.state[propName]));
+  function _getDeepFieldsetCopy(propName) {
+    const deepCopy = JSON.parse(JSON.stringify(fieldsets[propName]));
 
     return deepCopy;
   }
 
-  #getNotEmptyFieldsets() {
+  function _getNotEmptyFieldsets() {
     /* Returns an array without empty fieldsets.
     A user may create a fieldset but leave it blank completely.
     These fieldsets will not be displayed because they are empty */
     let deepStateCopy = {};
     let emptyFieldsets = [];
 
-    for (const prop in this.state) {
-      if (Array.isArray(this.state[prop])) {
-        deepStateCopy[prop] = this.#getDeepCopy(prop);
+    for (const prop in fieldsets) {
+      if (Array.isArray(fieldsets[prop])) {
+        deepStateCopy[prop] = _getDeepFieldsetCopy(prop);
       }
     }
 
@@ -133,63 +132,52 @@ class App extends React.Component {
     return deepStateCopy;
   }
 
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    const notEmptyFieldsets = this.#getNotEmptyFieldsets();
+    const notEmptyFieldsets = _getNotEmptyFieldsets();
 
-    this.setState({ ...notEmptyFieldsets, isDisplayingForm: false });
+    setFieldsets({ ...notEmptyFieldsets });
+    setIsDisplayingForm(false);
   }
 
-  handleResultEdit(e) {
-    this.setState({ isDisplayingForm: true });
+  function handleResultEdit(e) {
+    setIsDisplayingForm(true);
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App__header">
-          <h1 className="App__heading">CV Builder</h1>
-        </header>
-        {this.state.isDisplayingForm ? (
-          <DynamicForm
-            sections={{
-              general: this.state.general,
-              skills: this.state.skills,
-              education: this.state.education,
-              experience: this.state.experience,
-            }}
-            onNewInputCreation={this.handleNewInputCreation}
-            onRemoveInput={this.handleRemoveInput}
-            onInputChange={this.handleInputChange}
-            onSubmit={this.handleSubmit}
-          />
-        ) : (
-          <Result
-            sections={{
-              general: this.state.general,
-              skills: this.state.skills,
-              education: this.state.education,
-              experience: this.state.experience,
-            }}
-            onResultEdit={this.handleResultEdit}
-          />
-        )}
-        <footer className="App__footer">
-          Made with love by
-          <a target="_blank" href="https://github.com/twentysixhugs">
-            &nbsp;Iaan Lamanosau :3
-          </a>
-          <a
-            className="footer__code-link"
-            target="_blank"
-            href="https://github.com/twentysixhugs/CV-Builder"
-          >
-            View code
-          </a>
-        </footer>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App__header">
+        <h1 className="App__heading">CV Builder</h1>
+      </header>
+      {isDisplayingForm ? (
+        <DynamicForm
+          sections={{ ...fieldsets }}
+          onNewInputCreation={handleNewInputCreation}
+          onRemoveInput={handleRemoveInput}
+          onInputChange={handleInputChange}
+          onSubmit={handleSubmit}
+        />
+      ) : (
+        <Result
+          sections={{ ...fieldsets }}
+          onResultEdit={handleResultEdit}
+        />
+      )}
+      <footer className="App__footer">
+        Made with love by
+        <a target="_blank" href="https://github.com/twentysixhugs">
+          &nbsp;Iaan Lamanosau :3
+        </a>
+        <a
+          className="footer__code-link"
+          target="_blank"
+          href="https://github.com/twentysixhugs/CV-Builder"
+        >
+          View code
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
